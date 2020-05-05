f8759e139e2c8db5ed1759fce79f0829eab0695e
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-05-05 13:02:27 +0200

    Search ApiTV and It-en flags

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,8 @@ console.log('jQuery ok ->', $);
      */
 
     // refs
-    var api = 'https://api.themoviedb.org/3/search/movie';
+    var apiFilms = 'https://api.themoviedb.org/3/search/movie';
+    var apiSeries = 'https://api.themoviedb.org/3/search/tv';
     var inputSearch = $('.main-header input#search');
     var btnSearch = $('.main-header button.btn-search');
     var sectionResults = $('.main .main_result-search');
@@ -25,8 +26,11 @@ console.log('jQuery ok ->', $);
     // Avvia ricerca con bottone "Search"
     btnSearch.click( function() {
         
-        // Chiamata Api e stampo risultati
-        searchMovies(inputSearch, sectionResults, api, template);
+        // FILM Chiamata Api e stampo risultati
+        searchMovies(inputSearch, sectionResults, apiFilms, template);
+
+        // SERIETV Chiamata Api e stampo risu ltati
+        searchTV(inputSearch, sectionResults, apiSeries, template);
         
     });
 
@@ -38,9 +42,13 @@ console.log('jQuery ok ->', $);
              * usando quindi 
              */
             if(event.which == 13 || event.which == 32) {
-                searchMovies(inputSearch, sectionResults, api, template);
+                // FILM Chiamata Api e stampo risultati
+                searchMovies(inputSearch, sectionResults, apiFilms, template);
+                
+                // SERIETV Chiamata Api e stampo risu ltati
+                searchTV(inputSearch, sectionResults, apiSeries, template);
             };
-
+        
     });
 
     
@@ -54,11 +62,12 @@ console.log('jQuery ok ->', $);
  *  FUNCTIONS
  *********************/
 
+ // FILM - funzione per la ricerca dell'utente / gestisce anche chiamata API
  function searchMovies(inputSearch, sectionResults, api, template) {
     if(inputSearch.val() != '') {
 
         // pulisco sezione risultati per inserire nuovi dati
-        sectionResults.html('');
+        reset(sectionResults);
 
         // chiamata api con query in input
         $.ajax({
@@ -84,9 +93,13 @@ console.log('jQuery ok ->', $);
                         var context = {
                             title: thisResult.title,
                             originalTitle: thisResult.original_title,
-                            language: thisResult.original_language,
-                            average: thisResult.vote_average
+                            language: flags(thisResult),
+                            average: thisResult.vote_average,
+                            type: 'Film'
                         }
+
+                        console.log(thisResult.original_language);
+                        
 
                         //compilare e aggiungere template
                         var htmlMovie = template(context);
@@ -95,9 +108,9 @@ console.log('jQuery ok ->', $);
                     } 
 
                 } else {
-                    
-                    sectionResults.append('Nessun Risultato');
-                    
+                    reset(sectionResults);
+                    sectionResults.append('Nessun Risultato in Film');
+                    inputSearch.select();
                 }
                 
 
@@ -105,8 +118,94 @@ console.log('jQuery ok ->', $);
             },
                 
             error: function() {
-                console.log('Errore chiamata festività'); 
+                console.log('Errore chiamata'); 
             }
         });
+    } else {
+        reset(sectionResults);
+        sectionResults.append('Errore, inserire un valore nella ricerca')
+        inputSearch.focus();
     }
- }
+ };
+
+// SERIE TV - funzione per la ricerca dell'utente / gestisce anche chiamata API
+ function searchTV(inputSearch, sectionResults, api, template) {
+  if(inputSearch.val() != '') {
+
+      // pulisco sezione risultati per inserire nuovi dati
+     // reset(sectionResults);
+
+      // chiamata api con query in input
+      $.ajax({
+          url: api,
+          method: 'GET',
+          data: {
+              api_key: "e2330ecaa641a077ab62520c44ab636f",
+              language: "it-IT",
+              query: inputSearch.val()
+          },
+          success: function(res) {
+
+              // conservo i risultati in una variabile
+              var searchResults = res.results;
+
+              // controllo che effettivamente io abbia in risposta dei risultati
+              if(searchResults.length != 0) {
+
+                  for (var i = 0; i < searchResults.length; i++) {
+                      var thisResult = searchResults[i];
+                      
+                      // imposto dati template
+                      var context = {
+                          title: thisResult.name,
+                          originalTitle: thisResult.original_name,
+                          language: flags(thisResult),
+                          average: thisResult.vote_average,
+                          type: 'TV'
+                      }
+
+                      //compilare e aggiungere template
+                      var htmlMovie = template(context);
+                      sectionResults.append(htmlMovie);
+
+                  } 
+
+              } else {
+                  reset(sectionResults);
+                  sectionResults.append('Nessun Risultato in TV');
+                  inputSearch.select();
+              }
+              
+
+
+          },
+              
+          error: function() {
+              console.log('Errore chiamata'); 
+          }
+      });
+  } else {
+      reset(sectionResults);
+      sectionResults.append('Errore, inserire un valore nella ricerca')
+      inputSearch.focus();
+  };
+
+};
+
+function flags(thisResult) {
+    if(thisResult.original_language === 'it') {
+        return '<img src="assets/img/it.svg" alt="italian-flag">'
+        
+        
+
+    } else if (thisResult.original_language === 'en'){
+        return '<img src="assets/img/en.svg" alt="italian-flag">'
+    } else {
+        return thisResult.original_language;
+    }
+}
+
+// funzione Reset DOM container
+function reset(container) {  
+    container.html('');
+};      
