06dea9501d8cdf1acb58e50d1255da5a9323d23f
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-05-06 12:14:02 +0200

    all the work with APIs in one function "search"

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,11 +26,7 @@ console.log('jQuery ok ->', $);
     // Avvia ricerca con bottone "Search"
     btnSearch.click( function() {
         
-        // FILM Chiamata Api e stampo risultati
-        searchMovies(inputSearch, sectionResults, apiFilms, template);
-
-        // SERIETV Chiamata Api e stampo risu ltati
-        searchTV(inputSearch, sectionResults, apiSeries, template);
+        search(inputSearch, sectionResults, apiFilms, apiSeries, template)
         
     });
 
@@ -42,11 +38,7 @@ console.log('jQuery ok ->', $);
              * usando quindi 
              */
             if(event.which == 13 || event.which == 32) {
-                // FILM Chiamata Api e stampo risultati
-                searchMovies(inputSearch, sectionResults, apiFilms, template);
-                
-                // SERIETV Chiamata Api e stampo risu ltati
-                searchTV(inputSearch, sectionResults, apiSeries, template);
+                search(inputSearch, sectionResults, apiFilms, apiSeries, template)
             };
         
     });
@@ -62,16 +54,16 @@ console.log('jQuery ok ->', $);
  *  FUNCTIONS
  *********************/
 
- // FILM - funzione per la ricerca dell'utente / gestisce anche chiamata API
- function searchMovies(inputSearch, sectionResults, api, template) {
+//Funzione per la ricerca dell'utente / Con reset iniziale
+function search(inputSearch, sectionResults, apiFilms, apiTv, template, starAverage) {
+    reset(sectionResults);
+    
+    // FILM - ricerca dell'utente / gestisce chiamata API film
     if(inputSearch.val() != '') {
-
-        // pulisco sezione risultati per inserire nuovi dati
-        reset(sectionResults);
 
         // chiamata api con query in input
         $.ajax({
-            url: api,
+            url: apiFilms,
             method: 'GET',
             data: {
                 api_key: "e2330ecaa641a077ab62520c44ab636f",
@@ -87,63 +79,15 @@ console.log('jQuery ok ->', $);
                 if(searchResults.length != 0) {
 
                     for (var i = 0; i < searchResults.length; i++) {
+                        
                         var thisResult = searchResults[i];
                         
-                        // Assegno ad una varibile il voto in numero intero (arrotondato per eccesso)
-                        var intVote = Math.ceil(thisResult.vote_average)
-                        
-                        // Switch per assegnare un numero da 1 a 5 al voto
-                        switch(intVote) {
-                            case 0: 
-                                intVote = 0;
-                                break;
-                            case 1:
-                            case 2:
-                                intVote = 1;
-                                break;
-                            case 3:
-                            case 4:
-                                intVote = 2;
-                                break;
-                            case 5:
-                            case 6:
-                                intVote = 3;
-                                break;
-                            case 7:
-                            case 8:
-                                intVote = 4;
-                                break;
-                            case 9:
-                            case 10:
-                                intVote = 5;  
-                                break;
-                        }
-                        
-                        // Assegno il numero corrispondente di stelle piene
-                        var star = intVote;
-                        // Calcolo e assegno il numero corrispondente di stelle vuote
-                        var emptyStar = 5 - intVote;
-                        
-                        // Inizializzo variabile stringa
-                        var starAverage = '';
-
-                        // ciclo per concatenare le stelle piene
-                        for( var starCount = 1; starCount <= star; starCount++) {
-                            starAverage += '<i class="fas fa-star"></i>';
-                        }
-                        
-                        // ciclo per concatenare le stelle vuote
-                        for( var emptyStarCount = 1; emptyStarCount <= emptyStar; emptyStarCount++){
-                            starAverage += '<i class="far fa-star"></i>';
-                        }
-                        
-
                         // imposto dati template
                         var context = {
                             title: thisResult.title,
                             originalTitle: thisResult.original_title,
-                            language: flags(thisResult),
-                            average: starAverage,
+                            language: flags(thisResult.original_language),
+                            average: stars(starAverage, thisResult, searchResults),
                             type: 'Film'
                         }                      
 
@@ -172,129 +116,133 @@ console.log('jQuery ok ->', $);
         sectionResults.append('Errore, inserire un valore nella ricerca')
         inputSearch.focus();
     }
- };
-
-// SERIE TV - funzione per la ricerca dell'utente / gestisce anche chiamata API
- function searchTV(inputSearch, sectionResults, api, template) {
-  if(inputSearch.val() != '') {
-
-      // pulisco sezione risultati per inserire nuovi dati
-     // reset(sectionResults);
-
-      // chiamata api con query in input
-      $.ajax({
-          url: api,
-          method: 'GET',
-          data: {
-              api_key: "e2330ecaa641a077ab62520c44ab636f",
-              language: "it-IT",
-              query: inputSearch.val()
-          },
-          success: function(res) {
-
-              // conservo i risultati in una variabile
-              var searchResults = res.results;
-
-              // controllo che effettivamente io abbia in risposta dei risultati
-              if(searchResults.length != 0) {
-
-                  for (var i = 0; i < searchResults.length; i++) {
-                      var thisResult = searchResults[i];
-                      
-                      // Assegno ad una varibile il voto in numero intero (arrotondato per eccesso)
-                      var intVote = Math.ceil(thisResult.vote_average)
-                        
-                      // Switch per assegnare un numero da 1 a 5 al voto
-                      switch(intVote) {
-                          case 0: 
-                              intVote = 0;
-                              break;
-                          case 1:
-                          case 2:
-                              intVote = 1;
-                              break;
-                          case 3:
-                          case 4:
-                              intVote = 2;
-                              break;
-                          case 5:
-                          case 6:
-                              intVote = 3;
-                              break;
-                          case 7:
-                          case 8:
-                              intVote = 4;
-                              break;
-                          case 9:
-                          case 10:
-                              intVote = 5;  
-                              break;
-                      }
-                      
-                      // Assegno il numero corrispondente di stelle piene
-                      var star = intVote;
-                      // Calcolo e assegno il numero corrispondente di stelle vuote
-                      var emptyStar = 5 - intVote;
-                      
-                      // Inizializzo variabile stringa
-                      var starAverage = '';
-
-                      // ciclo per concatenare le stelle piene
-                      for( var starCount = 1; starCount <= star; starCount++) {
-                          starAverage += '<i class="fas fa-star"></i>';
-                      }
-                      
-                      // ciclo per concatenare le stelle vuote
-                      for( var emptyStarCount = 1; emptyStarCount <= emptyStar; emptyStarCount++){
-                          starAverage += '<i class="far fa-star"></i>';
-                      }
-                      // imposto dati template
-                      var context = {
-                          title: thisResult.name,
-                          originalTitle: thisResult.original_name,
-                          language: flags(thisResult),
-                          average: starAverage,
-                          type: 'TV'
-                      }
-
-                      //compilare e aggiungere template
-                      var htmlMovie = template(context);
-                      sectionResults.append(htmlMovie);
-
-                  } 
-
-              } else {
-                  reset(sectionResults);
-                  sectionResults.append('Nessun Risultato in TV');
-                  inputSearch.select();
-              }
-              
 
 
-          },
-              
-          error: function() {
-              console.log('Errore chiamata'); 
-          }
-      });
+// SERIE TV - ricerca dell'utente / gestisce chiamata API SerieTV
+    if(inputSearch.val() != '') {
+      
+        // chiamata api con query in input
+        $.ajax({
+            url: apiTv,
+            method: 'GET',
+            data: {
+                api_key: "e2330ecaa641a077ab62520c44ab636f",
+                language: "it-IT",
+                query: inputSearch.val()
+            },
+            success: function(res) {
+
+                // conservo i risultati in una variabile
+                var searchResults = res.results;
+
+                // controllo che effettivamente io abbia in risposta dei risultati
+                if(searchResults.length != 0) {
+
+                    for (var i = 0; i < searchResults.length; i++) {
+                        var thisResult = searchResults[i];
+                    
+                        // imposto dati template
+                        var context = {
+                            title: thisResult.name,
+                            originalTitle: thisResult.original_name,
+                            language: flags(thisResult.original_language),
+                            average: stars(starAverage, thisResult, searchResults),
+                            type: 'TV'
+                        }
+
+                        //compilare e aggiungere template
+                        var htmlMovie = template(context);
+                        sectionResults.append(htmlMovie);
+
+                    } 
+
+                } else {
+                    sectionResults.append('Nessun Risultato in TV');
+                    inputSearch.select();
+                }
+
+
+
+            },
+
+            error: function() {
+                console.log('Errore chiamata'); 
+            }
+        });
   } else {
-      reset(sectionResults);
       sectionResults.append('Errore, inserire un valore nella ricerca')
       inputSearch.focus();
   };
 
+
 };
 
-function flags(thisResult) {
-    if(thisResult.original_language === 'it') {
-        return '<img src="assets/img/it.svg" alt="italian-flag">'
-        
-    } else if (thisResult.original_language === 'en'){
-        return '<img src="assets/img/en.svg" alt="italian-flag">'
-    } else {
-        return thisResult.original_language;
-    }
+function stars(starAverage, thisResult, searchResults) {
+     // Assegno ad una varibile il voto in numero intero (arrotondato per eccesso)
+     var intVote = Math.ceil(thisResult.vote_average)
+                        
+     // Switch per assegnare un numero da 1 a 5 al voto
+     switch(intVote) {
+         case 0: 
+             intVote = 0;
+             break;
+         case 1:
+         case 2:
+             intVote = 1;
+             break;
+         case 3:
+         case 4:
+             intVote = 2;
+             break;
+         case 5:
+         case 6:
+             intVote = 3;
+             break;
+         case 7:
+         case 8:
+             intVote = 4;
+             break;
+         case 9:
+         case 10:
+             intVote = 5;  
+             break;
+     }
+     
+     // Assegno il numero corrispondente di stelle piene
+     var star = intVote;
+     // Calcolo e assegno il numero corrispondente di stelle vuote
+     var emptyStar = 5 - intVote;
+     
+     // Inizializzo variabile stringa
+     var starAverage = '';
+
+     // ciclo per concatenare le stelle piene
+     for( var starCount = 1; starCount <= star; starCount++) {
+         starAverage += '<i class="fas fa-star"></i>';
+     }
+     
+     // ciclo per concatenare le stelle vuote
+     for( var emptyStarCount = 1; emptyStarCount <= emptyStar; emptyStarCount++){
+         starAverage += '<i class="far fa-star"></i>';
+     }  
+     return starAverage;
 }
+
+function flags(thisResult) {
+    flagArray = [
+        'it',
+        'en'
+    ]
+
+    for (var i = 0; i < flagArray.length; i++) {
+        var ciao = flagArray[i]
+        if(thisResult == ciao) {
+            return '<img src="assets/img/' + ciao + '.svg"'
+        };  
+    };
+
+    return thisResult;  
+};
 
 // funzione Reset DOM container
 function reset(container) {  
